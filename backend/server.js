f6bc68f5a5ae2cb6599c79ed385a4dae46c3981e
AdeebIsmail <adeebi5412@gmail.com>
2020-11-06 19:18:40 -0600

    test push

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-//hello
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
